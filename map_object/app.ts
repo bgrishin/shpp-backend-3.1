11dd05974534ef213dfbe5f8aaf93b75add6e003
VERHOVNA-RADA <62828748+VERHOVNA-RADA@users.noreply.github.com>
2022-02-15 21:16:56 +0200

    [fix] without "as" & deleted extra comment

--- map_object/app.ts
+++ map_object/app.ts
@@ -1,13 +1,3 @@
-// function mapObject<Input, Arg, Func, NewObject>(obj: Input, func: (arg: Arg) => Func): NewObject {
-//     let newObject: any = {}
-//     for(let key in obj) {
-//         newObject[key] = func((obj[key] as unknown as Arg))
-//     }
-//     return (newObject as NewObject)
-// }
-//
-// const result = mapObject({ "roma" : 5, "vasya": 2 }, (x) => x > 3)
-// console.log(result)
 
 function mapObject<Object extends Record<string, any>, NewObject> (obj: Object, func: (val: Object[keyof Object]) => NewObject) {
     let NewObject: Partial<Record<keyof Object, NewObject>> = {}
