8156f222209039d1f4c2852cd75a97ab76c06b11
VERHOVNA-RADA <62828748+VERHOVNA-RADA@users.noreply.github.com>
2022-02-15 17:29:51 +0200

    [fix] better types

--- map_object/app.ts
+++ map_object/app.ts
@@ -1,9 +1,20 @@
-function mapObject<Input, Arg, Func, NewObject>(obj: Input, func: (arg: Arg) => Func): NewObject {
-    let newObject: any = {}
-    for(let key in obj) {
-        newObject[key] = func((obj[key] as unknown as Arg))
+// function mapObject<Input, Arg, Func, NewObject>(obj: Input, func: (arg: Arg) => Func): NewObject {
+//     let newObject: any = {}
+//     for(let key in obj) {
+//         newObject[key] = func((obj[key] as unknown as Arg))
+//     }
+//     return (newObject as NewObject)
+// }
+//
+// const result = mapObject({ "roma" : 5, "vasya": 2 }, (x) => x > 3)
+// console.log(result)
+
+function mapObject<Object extends Record<string, any>, Arg, Func, NewObject>(obj: Object, func: (x: Arg) => Func): NewObject {
+    let NewObject: NewObject | Record<string, any> = {}
+    for(let item in obj) {
+        NewObject[item] = func(obj[item])
     }
-    return (newObject as NewObject)
+    return (NewObject as NewObject)
 }
 
 const result = mapObject({ "roma" : 5, "vasya": 2 }, (x) => x > 2)
